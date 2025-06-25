99d848ce5b8df462f2dc141102ac1fc38d26472b
Manoj kumar <manoj166174@gmail.com>
2025-06-25 19:48:12 +0530

    Create Calci.py

---
+++ Calci.py
@@ -0,0 +1,8 @@
+def calculator(a, b, op):
+    if op == '+': return a + b
+    elif op == '-': return a - b
+    elif op == '*': return a * b
+    elif op == '/': return a / b
+    else: return "Invalid operator"
+
+print(calculator(10, 5, '+'))
