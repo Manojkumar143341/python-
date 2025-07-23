9a0b04c8c17b4e03a26180c4fc5b8f93d5510a00
Manoj kumar <manoj166174@gmail.com>
2025-07-23 21:55:23 +0530

    Add files via upload

---
+++ manoj.java
@@ -0,0 +1,12 @@
+public class manoj{
+    public static void main (String[] args){
+    int num = 28;
+    for(int i=0;i<=num;i++){
+        if(i%2==0 ){
+
+        }else{
+             System.out.println(i);
+        }
+    }
+    } 
+}
