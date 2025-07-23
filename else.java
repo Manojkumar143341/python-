2de6014587ec7363fa4cb5f0103b1c22d5474831
Manoj kumar <manoj166174@gmail.com>
2025-07-23 21:43:24 +0530

    Add files via upload

---
+++ else.java
@@ -0,0 +1,10 @@
+public class else{
+    public static void main (String[] args){
+    int num = 19;
+    if(num<=18){
+        System.out.println("hiii kolandha");
+    }else{
+    System.out.println("your going to vote");
+     }
+    }
+}
