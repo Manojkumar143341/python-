d57117f2a8588cd24976946cf918c9e14921fe7d
Manoj kumar <manoj166174@gmail.com>
2025-08-25 23:12:28 +0530

    Add files via upload

--- Home.jsx
+++ Home.jsx
@@ -1,51 +1,63 @@
-import React from 'react';
-
+import React,{useEffect,useState} from 'react'
+import Create from './Create'
+import axios from 'axios'
+import { Bs0CircleFill, BsFillCheckCircleFill } from 'react-icons/bs'
 function Home() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
-      <header className="bg-blue-600 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-lg font-bold">My Website</h1>
-          <nav>
-            <ul className="flex space-x-4">
-              <li>
-                <a href="#home" className="hover:underline">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#about" className="hover:underline">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="hover:underline">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+    const [todos , setTodos] = useState([])
+    useEffect(()=>{
+      axios.get('http://localhost:3001/get')
+      .then(result => setTodos(result.data))
+      .catch(err => console.log(err))
+    },[])
 
-      {/* Main Content */}
-      <main className="flex-grow container mx-auto p-4 text-center">
-        <h2 className="text-2xl font-bold mb-4">Welcome to the Home Page</h2>
-        <p className="text-gray-700">
-          This is a simple React application styled with Tailwind CSS. Explore
-          the website to learn more!
-        </p>
-      </main>
-
-      {/* Footer */}
-      <footer className="bg-blue-600 text-white p-4">
-        <div className="container mx-auto text-center">
-          <p>&copy; 2025 My Website. All rights reserved.</p>
-        </div>
-      </footer>
-    </div>
-  );
+const handleEdit=(id)=>{
+  axios.put('http://localhost:3001/update/'+id)
+  .then(result => {
+    location.reload()
+  })
+  .catch(err => console.log(err))
 }
 
-export default Home;
+const handleDelete =(id)=>{
+  axios.delete('http://localhost:3001/delete/'+id)
+  .then(result => {
+    location.reload()
+  })
+  .catch(err => console.log(err))
+}
+
+
+
+
+  return (
+    <div className='home'>
+
+<Create />
+
+{
+        todos.length === 0 
+        ?
+        <div>  <h2>no records </h2></div>
+        :
+        todos.map(todo =>(
+          <div className='task'>
+            <div className='checkbox' onClick={ ()=>handleEdit(todo._id)}>
+              { todo.done ? 
+              <BsFillCheckCircleFill className="icon"></BsFillCheckCircleFill>
+             : <Bs0CircleFill className="icon" />
+            }
+              
+               <p className={todo.done ? "line_through" : "" }>{todo.task}</p>
+             </div>
+             <div>
+              <span><Bs0CircleFill className='icon' onClick={ ()=> handleDelete(todo._id)}/></span>
+             </div>
+             </div>
+      
+      )) }
+
+    </div>
+  )
+}
+
+export default Home
