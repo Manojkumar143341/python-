b0901ae4dea2f1a4199a2b396b51a301e5bdce4c
Manoj kumar <manoj166174@gmail.com>
2025-09-04 23:24:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css"; // Import your CSS styles here
-import App from "./App"; // Main App component
-import { BrowserRouter as Router } from "react-router-dom"; // React Router for routing
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Rendering the app with routing
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
