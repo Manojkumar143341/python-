773c5f579cdce3ed3dd7436d207b2a2115d4d6a8
Manoj kumar <manoj166174@gmail.com>
2025-09-13 23:44:44 +0530

    Create War.py

---
+++ War.py
@@ -0,0 +1,10 @@
+# Simple Python Program
+
+# Print a message
+print("Hello, World!")
+
+# Take user input
+name = input("Enter your name: ")
+
+# Print a personalized message
+print("Welcome,", name, "! This is your first Python program.")
